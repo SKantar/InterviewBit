e5e2f6e3af29d02e96a824781166bace1266903d
skantar <sladjan.kantar@modoolar.com>
2018-02-08 22:17:14 +0100

    Find Permutation

--- 02_Arrays/find_permutation.py
+++ 02_Arrays/find_permutation.py
@@ -1,4 +1,27 @@
 # Find Permutation
+# https://www.interviewbit.com/problems/find-permutation/
+#
+# Given a positive integer n and a string s consisting only of letters D or I,
+# you have to find any permutation of first n positive integer that satisfy
+# the given input string.
+#
+# D means the next number is smaller, while I means the next number is greater.
+#
+# Notes
+#
+#     Length of given string s will always equal to n - 1
+#     Your solution should run in linear time and space.
+#
+# Example :
+# Input 1:
+#
+# n = 3
+#
+# s = ID
+#
+# Return: [1, 3, 2]
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 class Solution:
 
@@ -7,8 +30,11 @@ class Solution:
             A[i], A[j] = A[j], A[i]
             i, j = i + 1, j - 1
 
-    def findPermutation(self, A):
-        res = list(range(1, len(A) + 2))
+    # @param A : string
+    # @param B : integer
+    # @return a list of integers
+    def findPerm(self, A, B):
+        res = list(range(1, B + 1))
 
         start = None
 
@@ -31,9 +57,9 @@ class Solution:
 
 if __name__ == "__main__":
     s = Solution()
-    print(s.findPermutation("IIIII"))
-    print(s.findPermutation("IIDII"))
-    print(s.findPermutation("IIDID"))
-    print(s.findPermutation("DIDID"))
-    print(s.findPermutation("DDDII"))
-    print(s.findPermutation("I"))
+    print(s.findPerm("IIIII", 6))
+    print(s.findPerm("IIDII", 6))
+    print(s.findPerm("IIDID", 6))
+    print(s.findPerm("DIDID", 6))
+    print(s.findPerm("DDDII", 6))
+    print(s.findPerm("I", 2))
