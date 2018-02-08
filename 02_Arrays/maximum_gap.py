395f465a01096f70bddaa21f171337741010510b
skantar <sladjan.kantar@modoolar.com>
2018-02-08 21:56:07 +0100

    Maximum Consecutive Gap

--- 02_Arrays/maximum_gap.py
+++ 02_Arrays/maximum_gap.py
@@ -5,9 +5,17 @@
 #
 # Try to solve it in linear time/space.
 #
+# Example :
+
+# Input : [1, 10, 5]
+# Output : 5
+#
 # Return 0 if the array contains less than 2 elements.
 #
-# You may assume all elements in the array are non-negative integers and fit in the 32-bit signed integer range.
+# You may assume that all the elements in the array are non-negative
+# integers and fit in the 32-bit signed integer range.
+#
+# You may also assume that the difference will not overflow.
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
