bcbfd85e0f457563ffd59f176fe301f6ae7ae775
skantar <sladjan.kantar@modoolar.com>
2018-02-11 19:06:58 +0100

    FizzBuzz

--- 02_Math/fizz_buzz.py
+++ 02_Math/fizz_buzz.py
@@ -1,3 +1,17 @@
+# FizzBuzz
+# https://www.interviewbit.com/problems/fizzbuzz/
+#
+# Given a positive integer N, print all the integers from 1 to N. But for multiples of 3 print “Fizz” instead of the number and for the multiples of 5 print “Buzz”. Also for number which are multiple of 3 and 5, prints “FizzBuzz”.
+#
+# Example
+#
+# N = 5
+# Return: [1 2 Fizz 4 Buzz]
+#
+# Note: Instead of printing the answer, you have to return it as list of strings.
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 class Solution:
     def fizzBuzz(self, n):
         return [
