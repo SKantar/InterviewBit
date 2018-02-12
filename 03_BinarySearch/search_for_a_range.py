4c10b1bbc8e08e6a7de950d8db99a0b739c9fd8d
skantar <sladjan.kantar@modoolar.com>
2018-02-12 22:50:32 +0100

    Square Root of Integer

---
+++ 03_BinarySearch/search_for_a_range.py
@@ -0,0 +1,46 @@
+class Solution:
+
+    def bs_left(self, A, B):
+        """ Find left most """
+        low, high = 0, len(A) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if A[mid] == B:
+                if mid == 0 or A[mid - 1] != B:
+                    return mid
+                high = mid - 1
+            elif B < A[mid]:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return -1
+
+
+    def bs_right(self, A, B):
+        """ Find right most """
+
+        low, high = 0, len(A) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if A[mid] == B:
+                if mid + 1 >= len(A) or A[mid + 1] != B:
+                    return mid
+                low = mid + 1
+            elif B < A[mid]:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return -1
+
+    def searchRange(self, A, B):
+        return [self.bs_left(A, B), self.bs_right(A, B)]
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.searchRange([1], 1))
