f6b2bf860f0c7324b14cc36f3e083722051099d1
skantar <sladjan.kantar@modoolar.com>
2018-02-19 13:06:04 +0100

    Reverse Bits

--- 03_BitManipulation/reverse_bits.py
+++ 03_BitManipulation/reverse_bits.py
@@ -1,23 +1,50 @@
+# Reverse Bits
+# https://www.interviewbit.com/problems/reverse-bits/
+#
+# Reverse bits of an 32 bit unsigned integer
+#
+# Example 1:
+#
+# x = 0,
+#
+#           00000000000000000000000000000000
+# =>        00000000000000000000000000000000
+#
+# return 0
+#
+# Example 2:
+#
+# x = 3,
+#
+#           00000000000000000000000000000011
+# =>        11000000000000000000000000000000
+#
+# return 3221225472
+#
+# Since java does not have unsigned int, use long
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 class Solution:
-    # @param n, an integer
-    # @return an integer
-    def reverseBits(self, n):
+    # @param A : unsigned integer
+    # @return an unsigned integer
+    def reverse(self, A):
         head, tail = 1 << 31, 1
 
         for i in range(16):
-            not_same = int(n & head > 0) ^ int(n & tail > 0)
+            not_same = int(A & head > 0) ^ int(A & tail > 0)
 
             if not_same:
-                n = n ^ tail ^ head
+                A = A ^ tail ^ head
 
             head >>= 1
             tail <<= 1
-        return n
+        return A
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 if __name__ == "__main__":
     s = Solution()
-    print(s.reverseBits(43261596))
-    print(s.reverseBits(32768))
-    print(s.reverseBits(0))
+    print(s.reverse(43261596))
+    print(s.reverse(32768))
+    print(s.reverse(0))
