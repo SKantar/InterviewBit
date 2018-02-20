f7f32c953e8b688cc1e650c4a1a962f07a747599
skantar <sladjan.kantar@modoolar.com>
2018-02-20 10:00:07 +0100

    Palindrome String

--- 03_Strings/palindrome_string.py
+++ 03_Strings/palindrome_string.py
@@ -13,22 +13,46 @@
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+# class Solution:
+#     # @param A : string
+#     # @return an integer
+#     def _clear_string(self, A):
+#         return ''.join(c.lower() for c in A if c.isalnum())
+#
+#     def _is_palindrome(self, A):
+#         for i in range(len(A) // 2):
+#             if A[i] != A[len(A) - 1 - i]:
+#                 return False
+#         return True
+#
+#     def isPalindrome(self, A):
+#         A = self._clear_string(A)
+#         return 1 if self._is_palindrome(A) else 0
+
 class Solution:
     # @param A : string
     # @return an integer
     def isPalindrome(self, A):
         low, high = 0, len(A) - 1
-        while low < high:
+        while low <= high:
 
-            while low < high and not A[low].isalnum():
+            while low <= high and not A[low].isalnum():
                 low += 1
 
-            while low < high and not A[high].isalnum():
+            while low <= high and not A[high].isalnum():
                 high -= 1
 
             if A[low].lower() != A[high].lower():
                 return 0
 
-            low, hihj = low + 1, high - 1
+            low, high = low + 1, high - 1
 
         return 1
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.isPalindrome('A man, a plan, a canal: Panama"'))
+    print(s.isPalindrome('race a car'))
+
