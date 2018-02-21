7e035a277e53a204ca428d2e43419738ad7c0c22
skantar <skantar12@gmail.com>
2018-02-21 20:15:04 +0100

    Sort by Color

--- 03_TwoPointers/sort_by_color.py
+++ 03_TwoPointers/sort_by_color.py
@@ -1,4 +1,23 @@
+# Sort by Color
+# https://www.interviewbit.com/problems/sort-by-color/
+#
+# Given an array with n objects colored red, white or blue,
+# sort them so that objects of the same color are adjacent, with the colors in the order red, white and blue.
+#
+# Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+#
+# Note: Using library sort function is not allowed.
+#
+# Example :
+#
+# Input : [0 1 2 0 1 2]
+# Modify array so that it becomes : [0 0 1 1 2 2]
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 class Solution:
+    # @param A : list of integers
+    # @return A after the sort
     def sortColors(self, A):
         r, b = 0, len(A) - 1
         i = 0
