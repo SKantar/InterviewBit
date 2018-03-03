f4df56e6e415396fcc3f3fecb9ecc98b5f800e93
skantar <skantar12@gmail.com>
2018-03-03 12:18:11 +0100

    Equal

---
+++ 05_Hashing/equal.py
@@ -0,0 +1,59 @@
+# Equal
+# https://www.interviewbit.com/problems/equal/
+#
+# Given an array A of integers, find the index of values that satisfy A + B = C + D, where A,B,C & D are integers values in the array
+#
+# Note:
+#
+# 1) Return the indices `A1 B1 C1 D1`, so that
+#   A[A1] + A[B1] = A[C1] + A[D1]
+#   A1 < B1, C1 < D1
+#   A1 < C1, B1 != D1, B1 != C1
+#
+# 2) If there are more than one solutions,
+#    then return the tuple of values which are lexicographical smallest.
+#
+# Assume we have two solutions
+# S1 : A1 B1 C1 D1 ( these are values of indices int the array )
+# S2 : A2 B2 C2 D2
+#
+# S1 is lexicographically smaller than S2 iff
+#   A1 < A2 OR
+#   A1 = A2 AND B1 < B2 OR
+#   A1 = A2 AND B1 = B2 AND C1 < C2 OR
+#   A1 = A2 AND B1 = B2 AND C1 = C2 AND D1 < D2
+# Example:
+#
+# Input: [3, 4, 7, 1, 2, 9, 8]
+# Output: [0, 2, 3, 5] (O index)
+# If no solution is possible, return an empty list.
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+class Solution:
+
+    def intersect(self, l1, l2):
+        return [x for x in l1 if x in l2]
+
+    # @param A : list of integers
+    # @return a list of integers
+    def equal(self, A):
+        dp, ans = dict(), list()
+
+        for i in range(len(A)):
+            for j in range(i + 1, len(A)):
+                s, st = A[i] + A[j], [i, j]
+                if s in dp:
+                    if not self.intersect(dp[s], st):
+                        ll = list(dp[s] + st)
+                        ans = ll if not ans or ll < ans else ans
+                else:
+                    dp[s] = st
+
+        return ans
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.equal([3, 4, 7, 1, 2, 9, 8]))
