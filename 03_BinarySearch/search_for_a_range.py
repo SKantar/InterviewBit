ac468e3ca3ad1b2e3f4490157f9408764166576f
skantar <skantar12@gmail.com>
2018-05-19 21:21:20 +0200

    Search for a Range

--- 03_BinarySearch/search_for_a_range.py
+++ 03_BinarySearch/search_for_a_range.py
@@ -1,3 +1,23 @@
+# Search for a Range
+# https://www.interviewbit.com/problems/search-for-a-range/
+#
+# Given a sorted array of integers, find the starting and ending position of a given target value.
+#
+# Your algorithm’s runtime complexity must be in the order of O(log n).
+#
+# If the target is not found in the array, return [-1, -1].
+#
+# Example:
+#
+# Given [5, 7, 7, 8, 8, 10]
+#
+# and target value 8,
+#
+# return [3, 4].
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
 class Solution:
 
     def bs_left(self, A, B):
