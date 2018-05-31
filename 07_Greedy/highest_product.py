3e078d8e3f7c3068d7887bc26f053e94fc66d579
skantar <skantar12@gmail.com>
2018-05-31 11:47:36 +0200

    Highest Product

---
+++ 07_Greedy/highest_product.py
@@ -0,0 +1,30 @@
+# Highest Product
+# https://www.interviewbit.com/problems/highest-product/
+#
+# Given an array of integers, return the highest product possible by multiplying 3 numbers from the array
+#
+# Input:
+#
+# array of integers e.g {1, 2, 3}
+#
+#     NOTE: Solution will fit in a 32-bit signed integer
+#
+# Example:
+#
+# [0, -1, 3, 100, 70, 50]
+#
+# => 70*50*100 = 350000
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+class Solution:
+    # @param A : list of integers
+    # @return an integer
+    def maxp3(self, A):
+        A.sort()
+        return max(
+            A[-1] * A[-2] * A[-3],
+            A[-1] * A[-2] * A[0],
+            A[-1] * A[0] * A[1],
+            A[0] * A[1] * A[2],
+        )
